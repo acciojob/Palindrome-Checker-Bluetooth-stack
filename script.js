4a142a08878ac4084455440833dc1d226fe73798
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-09 19:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function palindrome(str){
 	let i = 0;
 	let j = str.length-1;
 	while(i<j){
-		if(str.charAt(i)!==str.charAt(j)){
-			return false;
-		}
-		else{
+		if(str.charAt(i)===str.charAt(j)){
 			i++;
 			j--
+		}
+		else{
+			return false;
 		}
 	}
 	return true;
